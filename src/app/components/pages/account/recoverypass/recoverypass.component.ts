c3720e798ace8b5315d362753839597d6bcd8d23
ClaudioAlarcon <claudio.alarcon@vyvsolutions.cl>
2024-05-11 23:07:07 -0400

    Some principal components created

---
+++ src/app/components/pages/account/recoverypass/recoverypass.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recoverypass',
+  standalone: true,
+  imports: [],
+  templateUrl: './recoverypass.component.html',
+  styleUrl: './recoverypass.component.scss'
+})
+export class RecoverypassComponent {
+
+}
